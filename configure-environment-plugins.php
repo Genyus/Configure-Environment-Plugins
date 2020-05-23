614eaed36e38756d7ba3e752f2e26e6d7a571192
Genyus <genyus@gmail.com>
2020-05-23 11:22:53 +0100

    chore: Increments version

--- configure-environment-plugins.php
+++ configure-environment-plugins.php
@@ -6,7 +6,7 @@
  * Author URI: https://ingenyus.com
  * Plugin URI: https://github.com/Genyus/Configure-Environment-Plugins
  * License: GPL version 2+
- * Version: 1.0.4
+ * Version: 1.0.5
  *
  * @package Configure-Environment-Plugins
  */
