cdaa1462708c6e98e01baa91dd4bc93ffb3ddec2
Genyus <genyus@gmail.com>
2024-03-23 17:56:22 -0400

    chore(deps): updates Composer dependencies
    Updates composer/installers
    
    BREAKING CHANGE: Updates composer/installers dependency from 1.x to 2.x

--- configure-environment-plugins.php
+++ configure-environment-plugins.php
@@ -6,7 +6,7 @@
  * Author URI: https://ingenyus.com
  * Plugin URI: https://github.com/Genyus/Configure-Environment-Plugins
  * License: GPL version 2+
- * Version: 1.0.8
+ * Version: 2.0.0
  *
  * @package Configure-Environment-Plugins
  */
